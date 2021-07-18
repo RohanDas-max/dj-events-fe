7f41106b948e199c3ed884fd634694c9bc1aa405
Rohan Das <67950918+RohanDas-max@users.noreply.github.com>
2021-07-18 23:47:44 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ const About = ({}) => {
     return (
         <Layout title='about'  >
          <h1 >Who Am I?</h1>
-         <span>Im a Learner, Developer and Gamer from a small town from NE-India {" "}</span>
+         <span>Im a Learner, Developer and Gamer from a small town from NE-India {" ."}</span>
          <span>I love New challanges, and love spending times learning basically anything based on Technology </span>
          <h4>Skills:- ReactJS, NextJS, MaterialUI, NodeJS, Heroku, Firebase, Vercel</h4>
         </Layout>
